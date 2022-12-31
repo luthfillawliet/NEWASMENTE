3ceb896cad560fe5157693beb1d21555bbe2e733
luthfillawliet <51342594+luthfillawliet@users.noreply.github.com>
2023-01-01 00:06:06 +0800

    Update main.py
    Berhasil eksekusi telegram dan eksekusi ct dr telegram hingga simpan foto ct, sisa kirim

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
 from ASMENTE import Asmente
+from parameter import Parameter
 import time
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, JobQueue
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+
+pm = Parameter()
+
+
+def start(update, context):
+    query = update.callback_query
+    context.bot.send_message(
+        chat_id=pm.chat_id, text="Bot Standby...")
+
+
+def read_command(update, context):
+    if (update.message.text[:2] == "ct"):
+        # Eksekusi buat CT
+        context.bot.send_message(
+            chat_id=pm.chat_id, text="Memulai pembuatan CT")
+        status, message = Asmente.buatCT(
+            id_pelanggan=update.message.text[3:15])
+        print("STATUS MAIN : ", message)
+    else:
+        print("command tidak dikenal")
+        context.bot.send_message(
+            chat_id=pm.chat_id, text="command tidak dikenal")
 
 
 def main():
-    Asmente.buatCT()
+    # bot Telegram
+    # Create updater and pass in Bot's auth key.
+    updater = Updater(token=pm.tokenbot, use_context=True)
+    # Get dispatcher to register handlers
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler(
+        'start', start, run_async=True))
+
+    # Message Handler harus d pasang paling terakhir
+    dispatcher.add_handler(MessageHandler(Filters.text, read_command))
+    # Start polling
+    updater.start_polling()
+    # Stop
+    updater.idle()
 
 
 if __name__ == '__main__':

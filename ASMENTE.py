9d267193a3b93266bbdd406f906174c118db2b9b
luthfillawliet <51342594+luthfillawliet@users.noreply.github.com>
2023-01-03 17:04:11 +0800

    Update ASMENTE.py
    Berhasil input keterangan, dan dataframe link dinamis untuk pengaduan

--- ASMENTE.py
+++ ASMENTE.py
@@ -16,53 +16,59 @@ class Asmente():
         # Cek buka web ap2t
         if (status == "yes"):
             [link_pengaduan, username_ap2t,
-                password_ap2t] = df.get_userlink_bykodeunit(kdunit=kodeunit)
-            print("Login username : ")
-            status = ap2t.login_ap2t(
-                username_ap2t=username_ap2t, password_ap2t=password_ap2t)
-            # cek login
-            if (status == "yes"):
-                print("Login Berhasil")
-                # Jika sukses, buat pengaduan
-                [status, message, nomoragenda] = ap2t.input_pengaduan_ct(
-                    id_pelanggan=id_pelanggan, petugas_dan_keterangan=keteranganCT, link_pengaduan_ct=link_pengaduan)
-                if (status == "yes" and int(nomoragenda) > 0):
-                    print(message, "dengan nomor Agenda : ", nomoragenda)
-                    [status, message, nomoragenda] = ap2t.tindakan_pengaduan_ct(
-                        nomor_agenda=nomoragenda, link_tindakan_pengaduan_ct=pm.linktindakanpengaduan)
+                password_ap2t] = df.get_userlink_bykodeunit(kdunit=kodeunit, jenis_user="TL TEKNIK", part_link_awal=pm.linkpengaduanct, part_link_akhir=pm.linkpengaduanct_2)
+            if (username_ap2t != "null" and password_ap2t != "0"):
+                print("Login username : ")
+                status = ap2t.login_ap2t(
+                    username_ap2t=username_ap2t, password_ap2t=password_ap2t)
+                # cek login
+                if (status == "yes"):
+                    print("Login Berhasil")
+                    # Jika sukses, buat pengaduan
+                    [status, message, nomoragenda] = ap2t.input_pengaduan_ct(
+                        id_pelanggan=id_pelanggan, petugas_dan_keterangan=keteranganCT, link_pengaduan_ct=link_pengaduan)
                     if (status == "yes" and int(nomoragenda) > 0):
-                        print(
-                            "Berhasil Entry tindakan pengaduan dengan nomor agenda : ", nomoragenda)
-                        [status, message, nomor_id] = ap2t.aktivasi_ct(
-                            nomoragenda=nomoragenda, link_aktivasi_meter=pm.linkaktivasimeter)
-                        print(message)
-                        if (status == "yes" and int(nomor_id) > 0):
-                            print("Eksekusi monitoring permohonan token")
-                            status, message = ap2t.monitoring_token(
-                                tipe_pencarian=0, nomor_id=id_pelanggan, url_monitoring_token=pm.link_montok, url_cetak_token=pm.baselink_kct)
-                            if (status == "yes"):
-                                print("Lanjut cetak Screenshoot")
-                                status, message = ap2t.take_screenshoot(
-                                    direktori=pm.filepathct, file_name="fotoct.png")
-                                return "yes", message
+                        print(message, "dengan nomor Agenda : ", nomoragenda)
+                        [status, message, nomoragenda] = ap2t.tindakan_pengaduan_ct(
+                            nomor_agenda=nomoragenda, link_tindakan_pengaduan_ct=pm.linktindakanpengaduan)
+                        if (status == "yes" and int(nomoragenda) > 0):
+                            print(
+                                "Berhasil Entry tindakan pengaduan dengan nomor agenda : ", nomoragenda)
+                            [status, message, nomor_id] = ap2t.aktivasi_ct(
+                                nomoragenda=nomoragenda, link_aktivasi_meter=pm.linkaktivasimeter)
+                            print(message)
+                            if (status == "yes" and int(nomor_id) > 0):
+                                print("Eksekusi monitoring permohonan token")
+                                status, message = ap2t.monitoring_token(
+                                    tipe_pencarian=0, nomor_id=id_pelanggan, url_monitoring_token=pm.link_montok, url_cetak_token=pm.baselink_kct)
+                                if (status == "yes"):
+                                    print("Lanjut cetak Screenshoot")
+                                    status, message = ap2t.take_screenshoot(
+                                        direktori=pm.filepathct, file_name="fotoct.png")
+                                    return "yes", message
+                                else:
+                                    message = "Gagal Eksekusi monitoring permohonan token"
+                                    print(message)
+                                    return "no", message
                             else:
-                                message = "Gagal Eksekusi monitoring permohonan token"
+                                message = "Gagal Aktivasi Token"
                                 print(message)
                                 return "no", message
-                        else:
-                            message = "Gagal Aktivasi Token"
-                            print(message)
-                            return "no", message
+                    else:
+                        message = "Gagal input pengaduan"
+                        print(message)
+                        return "no", message
+
                 else:
-                    message = "Gagal input pengaduan"
+                    message = "Gagal Login"
                     print(message)
                     return "no", message
-
             else:
-                message = "Gagal Login"
-                print(message)
+                message = "Gagal membuat link dan ambil Username serta password"
                 return "no", message
         else:
             message = "Gagal buka Web AP2T"
             print(message)
             return "no", message
+
+    #  def infopelanggan(tipe_pencarian : str, nomor_id:str):

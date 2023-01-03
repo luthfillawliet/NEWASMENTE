c018f2f8e7c0f5596ae8eda50d6871224ac158e6
luthfillawliet <51342594+luthfillawliet@users.noreply.github.com>
2023-01-03 17:05:09 +0800

    Update DataFrame.py
    Dinamic part link awal dan akhir berhasil

--- DataFrame.py
+++ DataFrame.py
@@ -6,99 +6,25 @@ pm = Parameter()
 
 class dataframe():
 
-    def get_userlink_bykodeunit(self, kdunit: str):
-        df = pd.read_excel(pm.filepathlistuser)
-        print(df)
-        if (kdunit == "32111"):
-            # print("Kode Unit terbaca : Panakkukang")
+    def get_userlink_bykodeunit(self, kdunit: str, jenis_user: str, part_link_awal: str, part_link_akhir: str):
+        try:
+            df = pd.read_excel(pm.filepathlistuser)
+            print(df)
             # get nip uSER
             list_username = df[df["KODE_UNIT"] == int(kdunit)]
             username = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["NIP"]
+                                     == jenis_user]["NIP"]
             password = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["Password_AP2T"]
-            link = pm.linkpengaduanct + \
-                username.item()+pm.linkpengaduanct_2
+                                     == jenis_user]["Password_AP2T"]
+            link = part_link_awal + \
+                username.item()+part_link_akhir
             # print("Username : ", username, "\nPassword : ", pasword)
             print(link)
             print("Length user : ", len(username.item()),
                   "\nLength password : ", len(password.item()), "(", password.item(), ")")
+            message = "Berhasil get link dan username - password"
             return link, username.item(), password.item()
-        elif (kdunit == "32121"):
-            print("Kode Unit terbaca : Mattoanging")
-            list_username = df[df["KODE_UNIT"] == int(kdunit)]
-            username = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["NIP"]
-            password = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["Password_AP2T"]
-            link = pm.linkpengaduanct + \
-                username.item()+pm.linkpengaduanct_2
-            # print("Username : ", username, "\nPassword : ", pasword)
-            print(link)
-            print("Length user : ", len(username.item()),
-                  "\nLength password : ", len(password.item()), "(", password.item(), ")")
-            return link, username.item(), password.item()
-        elif (kdunit == "32131"):
-            print("Kode Unit terbaca : Sungguminasa")
-            # print("Kode Unit terbaca : Panakkukang")
-            # get nip uSER
-            list_username = df[df["KODE_UNIT"] == int(kdunit)]
-            username = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["NIP"]
-            password = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["Password_AP2T"]
-            link = pm.linkpengaduanct + \
-                username.item()+pm.linkpengaduanct_2
-            # print("Username : ", username, "\nPassword : ", pasword)
-            print(link)
-            print("Length user : ", len(username.item()),
-                  "\nLength password : ", len(password.item()), "(", password.item(), ")")
-            return link, username.item(), password.item()
-        elif (kdunit == "32141"):
-            print("Kode Unit terbaca : Kalebajeng")
-            # print("Kode Unit terbaca : Panakkukang")
-            # get nip uSER
-            list_username = df[df["KODE_UNIT"] == int(kdunit)]
-            username = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["NIP"]
-            password = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["Password_AP2T"]
-            link = pm.linkpengaduanct + \
-                username.item()+pm.linkpengaduanct_2
-            # print("Username : ", username, "\nPassword : ", pasword)
-            print(link)
-            print("Length user : ", len(username.item()),
-                  "\nLength password : ", len(password.item()), "(", password.item(), ")")
-            return link, username.item(), password.item()
-        elif (kdunit == "32151"):
-            print("Kode Unit terbaca : Takalar")
-            # print("Kode Unit terbaca : Panakkukang")
-            # get nip uSER
-            list_username = df[df["KODE_UNIT"] == int(kdunit)]
-            username = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["NIP"]
-            password = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["Password_AP2T"]
-            link = pm.linkpengaduanct + \
-                username.item()+pm.linkpengaduanct_2
-            # print("Username : ", username, "\nPassword : ", pasword)
-            print(link)
-            print("Length user : ", len(username.item()),
-                  "\nLength password : ", len(password.item()), "(", password.item(), ")")
-            return link, username.item(), password.item()
-        elif (kdunit == "32161"):
-            print("Kode Unit terbaca : Malino")
-            # print("Kode Unit terbaca : Panakkukang")
-            # get nip uSER
-            list_username = df[df["KODE_UNIT"] == int(kdunit)]
-            username = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["NIP"]
-            password = list_username[list_username["Jabatan"]
-                                     == "TL TEKNIK"]["Password_AP2T"]
-            link = pm.linkpengaduanct + \
-                username.item()+pm.linkpengaduanct_2
-            # print("Username : ", username, "\nPassword : ", pasword)
-            print(link)
-            print("Length user : ", len(username.item()),
-                  "\nLength password : ", len(password.item()), "(", password.item(), ")")
-            return link, username.item(), password.item()
+        except Exception as e:
+            message = "Gagal ambil username, password dan get link, periksa File Excel dan kode unit\Message Error : " + e
+            print(message)
+            return link, "null", "0"

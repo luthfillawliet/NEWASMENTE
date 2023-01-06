7bb53ca61229fc5f09812304ca4f36b8949161fd
luthfillawliet <51342594+luthfillawliet@users.noreply.github.com>
2023-01-06 10:02:19 +0800

    Update main.py
    Berhasil autentikasi dan cek kode unit
    cuman error ketika request selain CT

--- main.py
+++ main.py
@@ -19,52 +19,66 @@ def start(update, context):
 
 
 def read_command(update, context):
+    # chat_id = pm.chat_id
+    chat_id = update.message.chat_id
     print(update.message.chat_id)
-    if (update.message.text[:2] == "ct" or update.message.text[:2] == "Ct" or update.message.text[:2] == "CT"):
-        # Eksekusi buat CT
-        context.bot.send_message(
-            chat_id=pm.chat_id, text="Memulai pembuatan CT Idpel : \n"+update.message.text[3:15]+"\nKode Unit : "+update.message.text[16:21]+"\nKeterangan : "+update.message.text[22:])
-        status, message = Asmente.buatCT(
-            id_pelanggan=update.message.text[3:15], kodeunit=update.message.text[16:21], keteranganCT=update.message.text[22:])
-        if (status == "yes"):
-            print("STATUS MAIN : ", message)
-            context.bot.send_message(chat_id=pm.chat_id, text=message)
-            time.sleep(2)
-            resp = requests.post(
-                "https://api.telegram.org/bot"+pm.tokenbot+"/sendPhoto?chat_id="+str(pm.chat_id), files=fm.send_photos(pm.files_foto_ct))
-        else:
-            print("STATUS MAIN : ", message)
-            context.bot.send_message(chat_id=pm.chat_id, text=message)
-    elif ((update.message.text[:4] == "info" and update.message.text[4:5] == "|") or (update.message.text[:4] == "Info" and update.message.text[4:5] == "|") or (update.message.text[:4] == "INFO" and update.message.text[4:5] == "|")):
-        if (update.message.text[5:6] == "0"):
+    [status, message] = Asmente.is_user_authenticated(chat_id=chat_id)
+    [status_kdunit, message_kdunit] = Asmente.kdunit_user(
+        chat_id=chat_id, kode_unit=update.message.text[16:21])
+    if (status == "yes" and status_kdunit == "yes"):
+        if (update.message.text[:2] == "ct" or update.message.text[:2] == "Ct" or update.message.text[:2] == "CT"):
+            # Eksekusi buat CT
             context.bot.send_message(
-                chat_id=pm.chat_id, text="Memulai Pencarian Informasi Idpel : \n"+update.message.text[7:])
-            [status, informasi, message] = Asmente.cek_infopelanggan(
-                tipe_pencarian="Id Pelanggan", nomor_id=update.message.text[7:], link_infopelanggan=pm.link_info_pelanggan)
+                chat_id=chat_id, text="Memulai pembuatan CT Idpel : \n"+update.message.text[3:15]+"\nKode Unit : "+update.message.text[16:21]+"\nKeterangan : "+update.message.text[22:])
+            status, message = Asmente.buatCT(
+                id_pelanggan=update.message.text[3:15], kodeunit=update.message.text[16:21], keteranganCT=update.message.text[22:])
+            if (status == "yes"):
+                print("STATUS MAIN : ", message)
+                context.bot.send_message(chat_id=chat_id, text=message)
+                time.sleep(2)
+                resp = requests.post(
+                    "https://api.telegram.org/bot"+pm.tokenbot+"/sendPhoto?chat_id="+str(chat_id), files=fm.send_photos(pm.files_foto_ct))
+            else:
+                print("STATUS MAIN : ", message)
+                context.bot.send_message(chat_id=chat_id, text=message)
+        elif ((update.message.text[:4] == "info" and update.message.text[4:5] == "|") or (update.message.text[:4] == "Info" and update.message.text[4:5] == "|") or (update.message.text[:4] == "INFO" and update.message.text[4:5] == "|")):
+            if (update.message.text[5:6] == "0"):
+                context.bot.send_message(
+                    chat_id=chat_id, text="Memulai Pencarian Informasi Idpel : \n"+update.message.text[7:])
+                [status, informasi, message] = Asmente.cek_infopelanggan(
+                    tipe_pencarian="Id Pelanggan", nomor_id=update.message.text[7:], link_infopelanggan=pm.link_info_pelanggan)
+                context.bot.send_message(
+                    chat_id=chat_id, text="Info Pelanggan berdasarkan Id Pelanggan :\n"+informasi)
+            elif (update.message.text[5:6] == "1"):
+                context.bot.send_message(
+                    chat_id=chat_id, text="Memulai Pencarian Informasi Nomor Meter :\n"+update.message.text[7:])
+                [status, informasi, message] = Asmente.cek_infopelanggan(
+                    tipe_pencarian="Nomor Meter", nomor_id=update.message.text[7:], link_infopelanggan=pm.link_info_pelanggan)
+                context.bot.send_message(
+                    chat_id=chat_id, text="Info Pelanggan berdasarkan Nomor Meter \n:"+informasi)
+            else:
+                context.bot.send_message(
+                    chat_id=chat_id, text="Tipe Pencarian tidak ditemukan")
+        elif (update.message.text[:8] == "infoacmt" and update.message.text[8:9] == "|" or update.message.text[:8] == "Infoacmt" and update.message.text[8:9] == "|" or update.message.text[:8] == "INFOACMT" and update.message.text[8:9] == "|"):
             context.bot.send_message(
-                chat_id=pm.chat_id, text="Info Pelanggan berdasarkan Id Pelanggan :\n"+informasi)
-        elif (update.message.text[5:6] == "1"):
+                chat_id=chat_id, text="Memulai pengecekan Info pelanggan ACMT idpel : "+update.message.text[9:])
+            [status, informasi, message] = Asmente.info_pelanggan_acmt(
+                id_pelanggan=update.message.text[9:])
             context.bot.send_message(
-                chat_id=pm.chat_id, text="Memulai Pencarian Informasi Nomor Meter :\n"+update.message.text[7:])
-            [status, informasi, message] = Asmente.cek_infopelanggan(
-                tipe_pencarian="Nomor Meter", nomor_id=update.message.text[7:], link_infopelanggan=pm.link_info_pelanggan)
-            context.bot.send_message(
-                chat_id=pm.chat_id, text="Info Pelanggan berdasarkan Nomor Meter \n:"+informasi)
-        else:
-            context.bot.send_message(
-                chat_id=pm.chat_id, text="Tipe Pencarian tidak ditemukan")
-    elif (update.message.text[:8] == "infoacmt" and update.message.text[8:9] == "|" or update.message.text[:8] == "Infoacmt" and update.message.text[8:9] == "|" or update.message.text[:8] == "INFOACMT" and update.message.text[8:9] == "|"):
-        context.bot.send_message(
-            chat_id=pm.chat_id, text="Memulai pengecekan Info pelanggan ACMT idpel : "+update.message.text[9:])
-        [status, informasi, message] = Asmente.info_pelanggan_acmt(
-            id_pelanggan=update.message.text[9:])
-        context.bot.send_message(
-            chat_id=pm.chat_id, text=informasi)
+                chat_id=chat_id, text=informasi)
 
-    else:
-        print("command tidak dikenal")
+        else:
+            print("command tidak dikenal")
+            context.bot.send_message(
+                chat_id=chat_id, text="command tidak dikenal")
+    elif (status == "yes" and status_kdunit == "no"):
+        message = "Periksa kode unit user..."
         context.bot.send_message(
-            chat_id=pm.chat_id, text="command tidak dikenal")
+            chat_id=chat_id, text=message)
+    else:
+        message = "Hubungi admin untuka autentikasi user..."
+        context.bot.send_message(
+            chat_id=chat_id, text=message)
 
 
 def main():
